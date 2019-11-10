824c090831969ea6b3c14163cc419711a567f343
Shalini Chaudhuri <shlnchaudhuri@gmail.com>
2019-11-10 11:08:12 -0500

    Alexnet model and testing data

--- code/alexnet.py
+++ code/alexnet.py
@@ -41,99 +41,104 @@ if __name__ == '__main__':
 
     best_accuracy_until_now = 0
 
-    if not paramsMetricFilePath.exists():
-        with open(paramsMetricFilePath, 'a+') as f:
-            writer = csv.writer(f)
-            writer.writerow(fields) 
+    # if not paramsMetricFilePath.exists():
+    #     with open(paramsMetricFilePath, 'a+') as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow(fields) 
 
-    for ctr, hyperparams in enumerate(hyperparamsArray):
-        learningRate = hyperparams['learningRate']
-        epochs = hyperparams['epochs']
-        momentum = hyperparams['momentum']
-        batchSize = hyperparams['batchSize']
+    # for ctr, hyperparams in enumerate(hyperparamsArray):
+    #     learningRate = hyperparams['learningRate']
+    #     epochs = hyperparams['epochs']
+    #     momentum = hyperparams['momentum']
+    #     batchSize = hyperparams['batchSize']
 
-        # read learning rate, momentum, batchSize and numEpochs from predefined csv
-        # store each separately
-        # batch_size = 8
-        # num_epochs = 15
-        # learningRate = 0.001
-        # momentum = 0.9
+    #     # read learning rate, momentum, batchSize and numEpochs from predefined csv
+    #     # store each separately
+    #     # batch_size = 8
+    #     # num_epochs = 15
+    #     # learningRate = 0.001
+    #     # momentum = 0.9
             
-        alexnetClassifier = Classifier(model_name, output_classes)
-        model = alexnetClassifier.initPretrainedModel(224)
+    #     alexnetClassifier = Classifier(model_name, output_classes)
+    #     model = alexnetClassifier.initPretrainedModel(224)
 
-    #     print(model)
+    # #     print(model)
 
-        dataloaders_dict, dataset_sizes, class_names = DataSet.initDataLoaders(data_dir, batchSize)
-        data_transforms = DataSet.setUpDataLoaderTransformers()
+    #     dataloaders_dict, dataset_sizes, class_names = DataSet.initDataLoaders(data_dir, batchSize)
+    #     data_transforms = DataSet.setUpDataLoaderTransformers()
         
 
-        image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val', 'test']}
+    #     image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val', 'test']}
 
-        # Detect if we have a GPU available
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #     # Detect if we have a GPU available
+    #     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         
-        sgdOptimizer = Optimizer(device)
-        optimizer_ft = sgdOptimizer.optimize(model, feature_extract, learningRate, momentum)
+    #     sgdOptimizer = Optimizer(device)
+    #     optimizer_ft = sgdOptimizer.optimize(model, feature_extract, learningRate, momentum)
 
-        criterion = nn.CrossEntropyLoss()
+    #     criterion = nn.CrossEntropyLoss()
 
-        model, train_acc_history, val_acc_history, best_accuracy = alexnetClassifier.train_model(model, 
-                criterion, 
-                optimizer_ft, 
-                dataloaders_dict, 
-                dataset_sizes)
-
-
-
-        with open(paramsMetricFilePath, 'a+') as f:
-            writer = csv.writer(f)
-            writer.writerow([learningRate, epochs, momentum, batchSize, best_accuracy]) 
-
-        plt.title("Validation Accuracy vs. Number of Epochs")
-        plt.xlabel("Epochs")
-        plt.ylabel("Validation Accuracy")
-        plt.plot(range(1,epochs+1), val_acc_history)
-        plt.legend()
-        # plt.show()
-
-        plt.savefig(metricsPath+'val_epoch_'+str(ctr)+'.png')
-        np.save(metricsPath+'val_epoch_'+str(ctr)+'.npy', val_acc_history)
+    #     model, train_acc_history, val_acc_history, best_accuracy = alexnetClassifier.train_model(model, 
+    #             criterion, 
+    #             optimizer_ft, 
+    #             dataloaders_dict, 
+    #             dataset_sizes)
 
 
-        plt.title("Training Accuracy vs. Number of Epochs")
-        plt.xlabel("Epochs")
-        plt.ylabel("Training Accuracy")
-        plt.plot(range(1,epochs+1), train_acc_history)
-        plt.legend()
-        # plt.show()
 
-        plt.savefig(metricsPath+'train_epoch_'+str(ctr)+'.png')
-        np.save(metricsPath+'train_epoch_'+str(ctr)+'.npy', train_acc_history)
+    #     with open(paramsMetricFilePath, 'a+') as f:
+    #         writer = csv.writer(f)
+    #         writer.writerow([learningRate, epochs, momentum, batchSize, best_accuracy]) 
+
+    #     plt.title("Validation Accuracy vs. Number of Epochs")
+    #     plt.xlabel("Epochs")
+    #     plt.ylabel("Validation Accuracy")
+    #     plt.plot(range(1,epochs+1), val_acc_history)
+    #     plt.clf()
+    #     plt.cla()
+
+    #     plt.savefig(metricsPath+'val_epoch_'+str(ctr)+'.png')
+    #     np.save(metricsPath+'val_epoch_'+str(ctr)+'.npy', val_acc_history)
+
+
+    #     plt.title("Training Accuracy vs. Number of Epochs")
+    #     plt.xlabel("Epochs")
+    #     plt.ylabel("Training Accuracy")
+    #     plt.plot(range(1,epochs+1), train_acc_history)
+    #     plt.clf()
+    #     plt.cla()
+
+    #     plt.savefig(metricsPath+'train_epoch_'+str(ctr)+'.png')
+    #     np.save(metricsPath+'train_epoch_'+str(ctr)+'.npy', train_acc_history)
         
-        if best_accuracy > best_accuracy_until_now:
-            print(best_accuracy)
-            best_accuracy_until_now = best_accuracy
+    #     if best_accuracy > best_accuracy_until_now:
+    #         print(best_accuracy)
+    #         best_accuracy_until_now = best_accuracy
 
-            # save only the best model until now
-            torch.save({
-                    'name': 'alexnetFeatureExtraction',
-                    'epoch': epochs,
-                    'learningRate': learningRate,
-                    'momentum': momentum,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer_ft.state_dict(),
-                    }, './trainedModels/alexnetFeatureExtraction.pt')
+    #         # save only the best model until now
+    #         torch.save({
+    #                 'name': 'alexnetFeatureExtraction',
+    #                 'epoch': epochs,
+    #                 'learningRate': learningRate,
+    #                 'momentum': momentum,
+    #                 'model_state_dict': model.state_dict(),
+    #                 'optimizer_state_dict': optimizer_ft.state_dict(),
+    #                 }, './trainedModels/alexnetFeatureExtraction.pt')
 
 
 
 #     # testing
-#     state = torch.load('./trainedModels/alexnetFeatureExtraction.pt')
-#     model.load_state_dict(state['model_state_dict'])
-#     predictions = alexnetClassifier.testModel(dataloaders_dict, model, class_names, dataset_sizes, batch_size)
+    alexnetClassifier = Classifier(model_name, output_classes)
+    model = alexnetClassifier.initPretrainedModel(224)
+    batchSize = 8
+    dataloaders_dict, dataset_sizes, class_names = DataSet.initDataLoaders(data_dir, batchSize)
+    data_transforms = DataSet.setUpDataLoaderTransformers()
+    state = torch.load('./trainedModels/alexnetFeatureExtraction.pt')
+    model.load_state_dict(state['model_state_dict'])
+    predictions = alexnetClassifier.testModel(dataloaders_dict, model, class_names, dataset_sizes, batchSize)
 
-#     # save predicted values
-#     np.savetxt('./trainedModels/predictedLabelsAlexNet.csv', predictions, fmt='%s')
+    # save predicted values
+    np.savetxt('./trainedModels/predictedLabelsAlexNet.csv', predictions, fmt='%s')
 
 
         
--- code/inceptionClassifier.py
+++ code/inceptionClassifier.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
+import json
+import csv
+from pathlib import Path
 
 from optimizer import Optimizer
 from classifier import Classifier
@@ -18,72 +21,116 @@ from dataset import DataSet
 print("PyTorch Version: ",torch.__version__)
 print("Torchvision Version: ",torchvision.__version__)
 
-
-
-
 if __name__ == '__main__':
-	# Top level data directory. Here we assume the format of the directory conforms
-	#   to the ImageFolder structure
-	data_dir = "./data"
-
-	# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
-	model_name = "inception"
-
-	# Number of classes in the dataset
-	num_classes = 2
-
-	# Batch size for training (change depending on how much memory you have)
-	batch_size = 8
-
-	# Number of epochs to train for
-	num_epochs = 15
-
-	# Flag for feature extracting. When False, we finetune the whole model,
-	#   when True we only update the reshaped layer params
+	data_dir = 'data'
+	model_name = 'inception'
+	output_classes = 2
 	feature_extract = True
 
-	inceptionClassifier = Classifier(model_name, num_classes)
-	model = inceptionClassifier.initPretrainedModel(299)
+	paramsFile = open('./code/paramsAlexnet.json')
+	paramsMetricFilePath = Path('./modelEvaluationMetrics/inception/inceptionMetrics.csv')
+	hyperparamsArray = json.load(paramsFile)
+	fields = ['learningRate', 'momentum', 'epochs', 'batchSize', 'valAccuracy']
+	metricsPath = './modelEvaluationMetrics/inception/'
 
-	# print(model)
+	best_accuracy_until_now = 0
 
-	dataloaders, dataset_sizes, class_names = DataSet.initDataLoaders(data_dir, batch_size)
-	data_transforms = DataSet.setUpDataLoaderTransformers(inputSize = 299)
+	if not paramsMetricFilePath.exists():
+		with open(paramsMetricFilePath, 'a+') as f:
+			writer = csv.writer(f)
+			writer.writerow(fields) 
 
-	image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
-	# Create training and validation dataloaders
-	dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
+	for ctr, hyperparams in enumerate(hyperparamsArray):
+		learningRate = hyperparams['learningRate']
+		epochs = hyperparams['epochs']
+		momentum = hyperparams['momentum']
+		batchSize = hyperparams['batchSize']
 
-	# Detect if we have a GPU available
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	print(device)
+		# read learning rate, momentum, batchSize and numEpochs from predefined csv
+		# store each separately
+		# batch_size = 8
+		# num_epochs = 15
+		# learningRate = 0.001
+		# momentum = 0.9
 
-	sgdOptimizer = Optimizer(device)
-	optimizer_ft = sgdOptimizer.optimize(model, feature_extract, 0.001, 0.9)
+		inceptionClassifier = Classifier(model_name, output_classes)
+		model = inceptionClassifier.initPretrainedModel(299)
 
-	criterion = nn.CrossEntropyLoss()
+    # #     print(model)
 
-	model, hist = inceptionClassifier.train_model(model, 
-	        criterion, 
-	        optimizer_ft, 
-	        dataloaders_dict, 
-	        dataset_sizes,
-	        True)
+		dataloaders_dict, dataset_sizes, class_names = DataSet.initDataLoaders(data_dir, batchSize)
+		data_transforms = DataSet.setUpDataLoaderTransformers(inputSize = 299)
+        
+
+		image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val', 'test']}
+		dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batchSize, shuffle=True, num_workers=4) for x in ['train', 'val']}
+
+        # Detect if we have a GPU available
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+		sgdOptimizer = Optimizer(device)
+		optimizer_ft = sgdOptimizer.optimize(model, feature_extract, learningRate, momentum)
+
+		criterion = nn.CrossEntropyLoss()
+
+		model, train_acc_history, val_acc_history, best_accuracy = inceptionClassifier.train_model(model, 
+			criterion, 
+			optimizer_ft, 
+			dataloaders_dict, 
+			dataset_sizes,
+			True)
 
 
-	torch.save({
-	        'name': 'inceptionV3FeatureExtraction',
-	        'epoch': 15,
-	        'model_state_dict': model.state_dict(),
-	        'optimizer_state_dict': optimizer_ft.state_dict(),
-	        }, '../trainedModels/inceptionV3FeatureExtraction.pt')
 
-	ohist = [h.cpu().numpy() for h in hist]
-    plt.title("Validation Accuracy vs. Number of Training Epochs")
-    plt.xlabel("Training Epochs")
-    plt.ylabel("Validation Accuracy")
-    plt.plot(range(1,num_epochs+1),ohist,label="Pretrained")
-    plt.ylim((0,1.))
-    plt.xticks(np.arange(1, num_epochs+1, 1.0))
-    plt.legend()
-    plt.show()
+
+		with open(paramsMetricFilePath, 'a+') as f:
+			writer = csv.writer(f)
+			writer.writerow([learningRate, epochs, momentum, batchSize, best_accuracy]) 
+
+		plt.title("Validation Accuracy vs. Number of Epochs")
+		plt.xlabel("Epochs")
+		plt.ylabel("Validation Accuracy")
+		plt.plot(range(1,epochs+1), val_acc_history)
+		plt.clf()
+		plt.cla()
+
+		plt.savefig(metricsPath+'val_epoch_'+str(ctr)+'.png')
+		np.save(metricsPath+'val_epoch_'+str(ctr)+'.npy', val_acc_history)
+
+
+		plt.title("Training Accuracy vs. Number of Epochs")
+		plt.xlabel("Epochs")
+		plt.ylabel("Training Accuracy")
+		plt.plot(range(1,epochs+1), train_acc_history)
+		plt.clf()
+		plt.cla()
+
+		plt.savefig(metricsPath+'train_epoch_'+str(ctr)+'.png')
+		np.save(metricsPath+'train_epoch_'+str(ctr)+'.npy', train_acc_history)
+        
+		if best_accuracy > best_accuracy_until_now:
+			print(best_accuracy)
+			best_accuracy_until_now = best_accuracy
+
+			# save only the best model until now
+			torch.save({
+				'name': 'inceptionV3FeatureExtraction',
+				'epoch': 15,
+				'model_state_dict': model.state_dict(),
+				'optimizer_state_dict': optimizer_ft.state_dict(),
+				}, './trainedModels/inceptionV3FeatureExtraction.pt')
+
+
+
+#     # testing
+    # inceptionClassifier = Classifier(model_name, output_classes)
+    # model = inceptionClassifier.initPretrainedModel(299)
+    # batchSize = 8
+    # dataloaders_dict, dataset_sizes, class_names = DataSet.initDataLoaders(data_dir, batchSize)
+    # data_transforms = DataSet.setUpDataLoaderTransformers()
+    # state = torch.load('./trainedModels/inceptionV3FeatureExtraction.pt')
+    # model.load_state_dict(state['model_state_dict'])
+    # predictions = alexnetClassifier.testModel(dataloaders_dict, model, class_names, dataset_sizes, batchSize)
+
+    # # save predicted values
+    # np.savetxt('./trainedModels/predictedLabelsInception.csv', predictions, fmt='%s')
